d47c8df0f0c3d9a0c71bede1c1586b0258ac68ca
genericFJS <freejailsymphonic@yahoo.de>
2018-01-23 17:03:39 +0100

    Update host-online-check script

--- scripts/acceptCert.py
+++ scripts/acceptCert.py
@@ -8,37 +8,49 @@ def ssh_check(host, user, password, timeout=30):
     host_out = host.replace('\r', '').replace('\n', '')
     # cmd = "hostname"
     options = ('-q -oStrictHostKeyChecking=no '
-               '-oUserKnownHostsFile=/dev/null -oPubkeyAuthentication=no')
+               '-oUserKnownHostsFile=/dev/null '
+               '-oPubkeyAuthentication=no'
+               )
     # ssh_cmd = 'ssh %s@%s %s "%s"' % (user, host, options, cmd)
     ssh_cmd = 'ssh %s@%s %s' % (user, host, options)
     try:
         child = pexpect.spawn(ssh_cmd, timeout=timeout)
+    except pexpect.exceptions.TIMEOUT:
+        print('{:10s} OFFLINE'.format(host_out))
+        return 1
+    try:
         child.expect(['Password: '])
 
         child.sendline(password)
+        print('{:10s} online (password entered)'.format(host_out))
+    except pexpect.exceptions.TIMEOUT:
+        if child.before.decode("utf-8") == "":
+            print('{:10s} OFFLINE'.format(host_out))
+            return 1
+        else:
+            print('{:10s} online (key accepted)'.format(host_out))
 
         # ssh_ret = child.read().decode("utf-8")
         # ssh_ret = ssh_ret.replace('\r', '').replace('\n', '')
-
-        print('{:10s} online'.format(host_out))
-        child.close()
-    except:
-        print('{:10s} offline !!!'.format(host_out))
-        return 1
-
+    child.close()
     return 0
 
 
 if __name__ == "__main__":
     os.chdir(os.path.dirname(os.path.abspath(sys.argv[0])))
-    user_inp = input("    user: ")
-    pw_inp = getpass.getpass('password: ')
+    # user_inp = input("    user: ")
+    # pw_inp = getpass.getpass('password: ')
+    user_inp = "s74053"
+    pw_inp = "***REMOVED***"
 
     working_machines = open('../machines/machinefile', 'w')
 
     with open("../machines/machinefileAll") as f:
-        for machine in f:
-            if ssh_check(machine, user_inp, pw_inp, 1) == 0:
-                working_machines.write(machine)
+        try:
+            for machine in f:
+                if ssh_check(machine, user_inp, pw_inp, 1) == 0:
+                    working_machines.write(machine)
+        except (KeyboardInterrupt, SystemExit):
+            print("Exiting...")
 
     working_machines.close()
