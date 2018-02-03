a9d29e6e51d09620902fad0a90e6fe7c0bbde1f3
genericFJS <freejailsymphonic@yahoo.de>
2018-02-03 18:02:00 +0100

    Add 2nd pass to ssh-password check

--- scripts/acceptCert.py
+++ scripts/acceptCert.py
@@ -14,16 +14,29 @@ def ssh_check(host, user, password, timeout=30):
                )
     # ssh_cmd = 'ssh %s@%s %s "%s"' % (user, host, options, cmd)
     ssh_cmd = 'ssh %s@%s %s' % (user, host, options)
+    # First pass:
     try:
         child = pexpect.spawn(ssh_cmd, timeout=timeout)
     except pexpect.exceptions.TIMEOUT:
-        print('{:10s} OFFLINE'.format(host_out))
+        pass
+    try:
+        child.expect(['Password: '])
+        child.sendline(password)
+    except pexpect.exceptions.TIMEOUT:
+        pass
+    child.close()
+    # Second pass:
+    try:
+        child = pexpect.spawn(ssh_cmd, timeout=timeout)
+    except pexpect.exceptions.TIMEOUT:
+        print('{:10s} OFFLINE (timeout)'.format(host_out))
         return 1
     try:
         child.expect(['Password: '])
 
         child.sendline(password)
         print('{:10s} online (password entered)'.format(host_out))
+        child.close()
     except pexpect.exceptions.TIMEOUT:
         # print(child.before.decode("utf-8"))
         if child.before.decode("utf-8") == "":
@@ -31,9 +44,6 @@ def ssh_check(host, user, password, timeout=30):
             return 1
         else:
             print('{:10s} online (key accepted)'.format(host_out))
-
-        # ssh_ret = child.read().decode("utf-8")
-        # ssh_ret = ssh_ret.replace('\r', '').replace('\n', '')
     child.close()
     return 0
 
@@ -50,7 +60,7 @@ if __name__ == "__main__":
             for machine in f:
                 if ssh_check(machine, user_inp, pw_inp, 1) == 0:
                     machine_ip = socket.gethostbyname(machine.strip())
-                    print('\tAdding ' + format(machine_ip) + ' as machine.')
+                    # print('\tAdding ' + format(machine_ip) + ' as machine.')
                     working_machines.write(machine_ip + "\n")
         except (KeyboardInterrupt, SystemExit):
             print("Exiting...")
